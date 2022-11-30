7f6f8de874a2dd3d282809d85fe5634f226fdb79
Songlin Ye <1379728314@qq.com>
2022-11-30 23:25:27 +0800

    note10

---
+++ note/note10.js
@@ -0,0 +1,11 @@
+
+
+var arr1 = [1, 2, 3];
+var arr2 = arr1;    // 浅拷贝
+var arr3 = [...arr1];   //    es6 深拷贝
+//  如果不用深拷贝， reverse()方法颠倒数组，会改变原数组，由于数组是复杂数据类型，
+//  原数组 arr1 改变会连带着 arr2 一起改变， 若想两个互不影响，需要用到深拷贝
+arr1.reverse();
+
+
+console.log(arr1,arr2,arr3);
