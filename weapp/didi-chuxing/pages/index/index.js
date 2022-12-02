a89635226fd6b950fb9f0cb1b18a39db73a6b765
Songlin Ye <1379728314@qq.com>
2022-12-02 20:53:46 +0800

    didi-chuxing

---
+++ weapp/didi-chuxing/pages/index/index.js
@@ -0,0 +1,207 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+      latitude: 23.099994,
+      longitude: 113.324520,
+      index: 0,
+      menu_type:0,
+      func1: [
+        {
+          id: 1,
+          url: "/image/011.jpg",
+          name: "打车"
+        },
+        {
+          id: 2,
+          url: "/image/012.jpg",
+          name: "代驾"
+        },
+        {
+          id: 3,
+          url: "/image/013.jpg",
+          name: "青桔骑行"
+        },
+        {
+          id: 4,
+          url: "/image/014.jpg",
+          name: "顺风车"
+        },
+        {
+          id: 5,
+          url: "/image/015.jpg",
+          name: "优惠加油"
+        },
+        {
+          id: 6,
+          url: "/image/021.jpg",
+          name: "特价拼车"
+        },
+        {
+          id: 7,
+          url: "/image/022.jpg",
+          name: "远途拼车"
+        },
+        {
+          id: 8,
+          url: "/image/023.jpg",
+          name: "天天神券"
+        },
+        {
+          id: 9,
+          url: "/image/024.jpg",
+          name: "借钱"
+        },
+        {
+          id: 10,
+          url: "/image/025.jpg",
+          name: "省钱套餐"
+        },
+        
+      ],
+      func2: [
+        {
+          id: 11,
+          url: "/image/111.jpg",
+          name: "搬家"
+        },
+        {
+          id: 12,
+          url: "/image/112.jpg",
+          name: "专车"
+        },
+        {
+          id: 13,
+          url: "/image/113.jpg",
+          name: "核酸检测"
+        },
+        {
+          id: 14,
+          url: "/image/114.jpg",
+          name: "消赚现金"
+        },
+        {
+          id: 15,
+          url: "/image/115.jpg",
+          name: "公交"
+        },
+        {
+          id: 16,
+          url: "/image/121.jpg",
+          name: "走路赚钱"
+        },
+        {
+          id: 17,
+          url: "/image/122.jpg",
+          name: "天天有赚"
+        },
+        {
+          id: 18,
+          url: "/image/123.jpg",
+          name: "守护候鸟"
+        },
+        {
+          id: 19,
+          url: "/image/124.jpg",
+          name: "养券大师"
+        },
+        {
+          id: 20,
+          url: "/image/125.jpg",
+          name: "充值中心"
+        },
+        {
+          id: 21,
+          url: "/image/131.jpg",
+          name: "滴滴果园"
+        },
+        {
+          id: 22,
+          url: "/image/132.jpg",
+          name: "小桔充电"
+        },
+        {
+          id: 23,
+          url: "/image/133.jpg",
+          name: "助老打车"
+        },
+        {
+          id: 24,
+          url: "/image/134.jpg",
+          name: "车品商城"
+        },
+        {
+          id: 25,
+          url: "/image/135.jpg",
+          name: "保险"
+        },
+        {
+          id: 26,
+          url: "/image/141.jpg",
+          name: "严选车品"
+        },
+        {
+          id: 27,
+          url: "/image/142.jpg",
+          name: "路况查询"
+        }
+        
+      ]
+  },
+  // 事件处理函数
+  bindViewTap() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  // 滑动事件
+  change(event) {      // 函数定义用冒号
+    // console.log(event)
+    let current = event.detail.current;
+    this.setData({
+      menu_type: current
+    })
+  },
+  onLoad() {
+    wx.showLoading({
+      title: '加载中...',
+    })
+    wx.getLocation({
+      type:'gcj02',
+      success:(res) => {
+       // if (res.statusCode === 200) {    //  getLocation 没有状态码
+        wx.hideLoading();
+        let { latitude, longitude} = res;
+        //console.log(latitude,longitude)
+        this.setData({
+          longitude: longitude,
+          latitude: latitude
+        })
+     // }
+      }
+    })
+  },
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    })
+  },
+  getUserInfo(e) {
+    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
+    console.log(e)
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
---
+++ weapp/didi-chuxing/readme.md
@@ -0,0 +1,5 @@
+
+
+
+- indicator-active-color    当前页面指示点颜色
+     indicator-color        指示点颜色默认颜色（不包括当前）
