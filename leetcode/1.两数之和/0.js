7c494502a85094bfe9783f9c6242b1c3a35eb396
Songlin Ye <1379728314@qq.com>
2022-12-19 23:23:10 +0800

    add

--- leetcode/1.两数之和/0.js
+++ leetcode/1.两数之和/0.js
@@ -1,9 +1,12 @@
 var mymap = new Map();
 console.log(mymap);  
 mymap.set("a","b");
+mymap.set("e");     //  插入
 console.log(mymap);
 mymap.set(1,"c");
 console.log(mymap);
-console.log(mymap.get(1));
-console.log(mymap.has("a"));
+console.log(mymap.get(1));    //  获取
+console.log(mymap.has("a"));   //  查找判断
 console.log(mymap.has("b"));
+mymap = new Map();      // 清空
+console.log(mymap);
--- leetcode/2.两数相加/1.js
+++ leetcode/2.两数相加/1.js
@@ -1,15 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
 var addTwoNumbers = function(l1, l2) {
-    l1.reverse();
-    l2.reverse();
-    let a = Number(l1.join(""));
-    let b = Number(l2.join(""));
-    let sum = a + b;
-    let l3 = sum.toString().split("");
-    l3.reverse();
-    return l3;
+    let sum = new ListNode(0);
+    let addOne = 0;
+    let head = sum;
+    while(addOne == 1 || l1 || l2) {
+        let val1 = (l1 !== null) ? l1.val : 0;
+        let val2 = (l2 !== null) ? l2.val : 0;
+        let r1 = val1 + val2 + addOne;
+        addOne = (r1 >= 10) ? 1 : 0;
+        sum.next = new ListNode(r1 % 10);
+        sum = sum.next;
+        if(l1) l1 = l1.next;
+        if(l2) l2 = l2.next;
+    }
+    return head.next;
 };
-
-
-console.log(addTwoNumbers([9,9,9,9,9,9,9],[9,9,9,9]));
-
-// join 不改变原数组
