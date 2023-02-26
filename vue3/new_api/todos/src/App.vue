08c9611ec51876192e45ca30c7ca15677c8998a7
Songlin Ye <1379728314@qq.com>
2023-02-26 09:41:30 +0800

    add

---
+++ vue3/new_api/todos/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- <h1 @click="add">{{ count }}  ------- {{ state.count }}</h1> -->
+    <h1 @click="add" class="red">{{ count }}</h1>
+  </div>
+</template>
+<script setup>
+import { ref } from 'vue';
+// ref 响应式   接收简单值
+let count = ref(1);
+console.log(count,'///')
+// let state = reactive({  //  reactive  响应式   接收对象
+//   count: 1
+// })
+function add() {
+  count.value++
+}
+
+</script>
+<style>
+  .red {
+    color: red;
+  }
+</style>
