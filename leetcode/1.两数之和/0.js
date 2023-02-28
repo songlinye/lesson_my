b374d21144207c6061d64be14f4b144ac923061b
Songlin Ye <1379728314@qq.com>
2023-02-28 22:06:11 +0800

    add

--- leetcode/1.两数之和/0.js
+++ leetcode/1.两数之和/0.js
@@ -6,6 +6,7 @@ console.log(mymap);
 mymap.set(1,"c");
 console.log(mymap);
 console.log(mymap.get(1));    //  获取
+console.log(mymap.has(1));
 console.log(mymap.has("a"));   //  查找判断
 console.log(mymap.has("b"));
 mymap = new Map();      // 清空
