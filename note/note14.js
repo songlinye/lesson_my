8c40d601adbeeed323d8c615eee8a5f84e7e2527
Songlin Ye <1379728314@qq.com>
2023-03-02 23:06:40 +0800

    add

---
+++ note/note14.js
@@ -0,0 +1,7 @@
+//    match() 方法可在字符串内检索指定的值，或找到一个或多个正则表达式的匹配。
+
+
+var str="The rain in SPAIN stays mainly in the plaiN"; 
+var n=str.match(/ain/g);    // [ 'ain', 'ain' ]
+var n=str.match(/ain/gi);   // [ 'ain', 'AIN', 'ain', 'aiN' ]
+console.log(n)
